0cda03735019a9202e077a07c516a53d99d3b1bb
sjones6 <jones.spencera@gmail.com>
2017-10-04 08:53:01 -0400

    Enable require funcatron directly

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-module.exports = {
-    funcatron: require("./src/funcatron"),
-    make: require("./src/make"),
-    group: require("./src/group"),
-    pipe: require("./src/pipe"),
-    stack: require("./src/stack")
-}
+const f = require("./src/funcatron")
+
+f.funcatron = f
+f.make = require("./src/make")
+f.group = require("./src/group")
+f.pipe = require("./src/pipe")
+f.stack = require("./src/stack")
+
+module.exports = f
